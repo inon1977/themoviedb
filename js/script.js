28edaaf1588ec0c41b843a8a1aa10c0cee766bd7
inon1977 <inonshnayder@Inons-Mac-mini.local>
2023-08-24 22:13:58 -0700

    cleanup

--- js/script.js
+++ js/script.js
@@ -8,25 +8,8 @@ const apiUrl = 'https://api.themoviedb.org/3/movie/popular';
             data.results.forEach(movie => {
                 const genreIds = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37];
                 const genres = {
-                28: "Action",
-                12: "Adventure",
-                16: "Animation",
-                35: "Comedy",
-                80: "Crime",
-                99: "Documentary",
-                18: "Drama",
-                10751: "Family",
-                14: "Fantasy",
-                36: "History",
-                27: "Horror",
-                10402: "Music",
-                9648: "Mystery",
-                10749: "Romance",
-                878: "Science Fiction",
-                10770: "TV Movie",
-                53: "Thriller",
-                10752: "War",
-                37: "Western"
+                28: "Action",12: "Adventure", 16: "Animation",35: "Comedy",80: "Crime",99: "Documentary",18: "Drama",
+                10751: "Family",14: "Fantasy",36: "History",27: "Horror",10402: "Music",9648: "Mystery",10749: "Romance",878: "Science Fiction",10770: "TV Movie",53: "Thriller",10752: "War",37: "Western"
                 };
                 let movie_Genr = '';
                 let movieGenre = movie.genre_ids;
@@ -184,8 +167,6 @@ const apiUrl = 'https://api.themoviedb.org/3/movie/popular';
                                                     castContainer.appendChild(castListTitle);    
                                                     castContainer.appendChild(castList);
                                 modalBody.appendChild(modalFooter);
-                        
-            
         });
       })
       .catch(error => {
